3a06d362b50795b2aed70960a4f3ad157a1a2034
joseph <i.k.zubarev@gmail.com>
2013-09-21 16:57:25 +0400

    Diagrams started to show up

--- npnets.graphicaleditor/src/npnets/graphicaleditor/editor/command/NodeCreateCommand.java
+++ npnets.graphicaleditor/src/npnets/graphicaleditor/editor/command/NodeCreateCommand.java
@@ -5,30 +5,32 @@ import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.commands.Command;
 
+import ru.mathtech.npntool.npnets.highlevelnets.hlpn.HLPNFactory;
+import ru.mathtech.npntool.npnets.highlevelnets.hlpn.Node;
 import ru.mathtech.npntool.npnets.npndiagrams.NPNDiagramNetSystem;
 import ru.mathtech.npntool.npnets.npndiagrams.NPNSymbolNodeSN;
+import ru.mathtech.npntool.npnets.npndiagrams.NPNSymbolPlaceSN;
+import ru.mathtech.npntool.npnets.npndiagrams.NPNSymbolTransitionSN;
 //import ru.mathtech.npntool.npnets.highlevelnets.hlpn.HighLevelPetriNet;
-//import ru.mathtech.npntool.npnets.highlevelnets.hlpn.Node;
  
 public class NodeCreateCommand extends Command {
  
   private static final Dimension defaultDimension = new Dimension(50, 50);
-  //private Node newNode;
   private NPNSymbolNodeSN newNode;
   private Point coordinates;
   private NPNDiagramNetSystem net;
-  //private HighLevelPetriNet net;
  
   @Override public void execute() {
     if(coordinates != null) {
       newNode.setConstraints(new Rectangle(coordinates, defaultDimension));
     }
-    //newNode.setNet(net);
+    Node newModel = newNode instanceof NPNSymbolPlaceSN ? HLPNFactory.eINSTANCE.createPlace() : HLPNFactory.eINSTANCE.createTransition();
+    newNode.setModel(newModel);
     newNode.setDiagram(net);
+    newNode.getDiagram().getModel().getNodes().add(newModel);
   }
  
   @Override public void undo() {
-    //newNode.setNet(null);
 	  newNode.setDiagram(null);
   }
  
